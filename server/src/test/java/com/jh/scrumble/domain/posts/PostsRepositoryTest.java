dd05ad91f1da787d53c6b90a5272a526274d72dd
JH <oh.ji-hoon@daum.net>
2022-05-01 22:53:55 +0900

    기초 세팅

--- server/src/test/java/com/jh/scrumble/domain/posts/PostsRepositoryTest.java
+++ server/src/test/java/com/jh/scrumble/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class PostsRepositoryTest {
 
     @Autowired
