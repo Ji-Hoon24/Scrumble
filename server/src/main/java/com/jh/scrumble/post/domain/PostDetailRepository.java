f51c009c2a82f394f196f4e153d2ddcc193de117
Ji-Hoon24 <oh.ji.hoon24@gmail.com>
2022-07-07 20:33:36 +0900

    [기능] : 특정 포스트 삭제 기능 추가

--- server/src/main/java/com/jh/scrumble/post/domain/PostDetailRepository.java
+++ server/src/main/java/com/jh/scrumble/post/domain/PostDetailRepository.java
@@ -2,6 +2,7 @@ package com.jh.scrumble.post.domain;
 
 import com.jh.scrumble.post.dto.entity.PostDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,4 +11,8 @@ import java.util.List;
 public interface PostDetailRepository extends JpaRepository<PostDetail, Long> {
     @Query("select pd from PostDetail pd where pd.id = :id")
     List<PostDetail> findAllById(@Param(value="id") Long id);
+
+    @Modifying
+    @Query("delete from PostDetail pd where pd.id = :id")
+    void deleteAllById(@Param(value="id") Long id);
 }
--- server/src/main/java/com/jh/scrumble/post/service/PostsService.java
+++ server/src/main/java/com/jh/scrumble/post/service/PostsService.java
@@ -56,6 +56,7 @@ public class PostsService {
     @Transactional
     public void delete(Long id) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postDetailRepository.deleteAllById(id);
         postsRepository.delete(posts);
     }
 }
